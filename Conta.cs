2212e0c85ce4408c3fe30141e5927fc34b0c676f
Veridiano <veridianobarroso@gmail.com>
2023-03-19 16:51:22 -0500

    Polimorfismo

--- Conta.cs
+++ Conta.cs
@@ -10,10 +10,22 @@ namespace CursoEngenhariaCS
         public double Saldo { get; protected set; }
 
         public void Deposita(double valor){
+
             this.Saldo += valor;
+            
+            System.Console.WriteLine($"Depositou: {this.Saldo}");
+            
         }
         public virtual void Saca(double valor){
-            this.Saldo = this.Saldo - valor -1;
+            this.Saldo = this.Saldo - valor - 1;
+            this.Saldo = Math.Round(this.Saldo, 2);
+            System.Console.WriteLine($"Sacou: {valor} -> Taxa: 1,00 -> Saldo: {this.Saldo}");
+        }
+        public virtual void Saca(double valor, double taxa){
+            
+            this.Saldo = this.Saldo - valor - (valor * taxa)/100;
+            this.Saldo = Math.Round(this.Saldo, 2);
+            System.Console.WriteLine($"Sacou: {valor} -> Taxa: {taxa}% -> Saldo: {this.Saldo}");
         }
     }
     public class Especial : Conta{
@@ -21,5 +33,6 @@ namespace CursoEngenhariaCS
         {
              this.Saldo = this.Saldo - valor -3;
         }
+
     }
 }
