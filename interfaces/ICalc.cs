b7c0cc6cd4d0523a64a4ec11ee2236492d1e9172
Veridiano <veridianobarroso@gmail.com>
2023-03-20 18:48:24 -0500

    Interfaces

---
+++ interfaces/ICalc.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoEngenhariaCS.interfaces
+{
+    public interface ICalc
+    {
+        
+        public int Somar(int num1, int num2);
+        public int Substrair(int num1, int num2);
+
+    }
+}
